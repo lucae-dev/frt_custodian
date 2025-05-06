b2a67d1b8cb5c81ece661f53f52ea46725af6f49
lucaortolomo <l.ortolomo@gmail.com>
2025-05-06 20:54:33 +0200

    add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@
 import React from 'react';
 import ChatTerminal from './Terminal.jsx';
 import './styles.css';
+import { Analytics } from '@vercel/analytics/react';
+
 
 export default function App() {
-  return <ChatTerminal />;
+  return  (<><ChatTerminal /><Analytics /></>); 
 }
